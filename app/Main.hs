b6e67415352d7d6433ddcc8873211d51c9bc4466
Emre YILMAZ <emreyilmaz@yandex.com>
2020-03-05 00:24:37 +0300

    Data.Text won't works with read, so changed to Data.Text.pack function

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,11 @@ import           Web.Scotty
 import           System.Environment
 import           Data.Monoid                    ( mconcat )
 import           App
-
+import qualified Data.Text                     as T
 
 main :: IO ()
 main = do
   githubClientKey    <- getEnv "CLIENT_KEY"
   githubClientSecret <- getEnv "CLIENT_SECRET"
   port               <- getEnv "PORT"
-  app $ read port $ read githubClientKey $ read githubClientSecret
-
+  app (read port) (T.pack githubClientKey) (T.pack githubClientSecret)
