e0700acec04d3fdec165210ad8687b1193c24433
Emre YILMAZ <emreyilmaz@yandex.com>
2020-03-05 00:23:53 +0300

    bslToText method created

--- src/Utils.hs
+++ src/Utils.hs
@@ -7,6 +7,7 @@ import qualified Data.Text.Lazy                as TL
 import qualified Data.Text.Encoding            as TE
 import           URI.ByteString
 import           Web.Scotty.Internal.Types
+import qualified Data.ByteString.Lazy.Char8    as BSL
 
 
 createCodeUri :: OAuth2 -> [(ByteString, ByteString)] -> Text
@@ -23,3 +24,6 @@ paramValue key = fmap snd . filter (hasParam key)
 
 hasParam :: Text -> Param -> Bool
 hasParam t = (== t) . fst
+
+bslToText :: BSL.ByteString -> TL.Text
+bslToText = TL.pack . BSL.unpack
