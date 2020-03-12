767882083d32008c88e66676c93ed8f4093d5c91
Emre YILMAZ <emreyilmaz@yandex.com>
2020-03-12 23:50:15 +0300

    bk

--- src/Session.hs
+++ src/Session.hs
@@ -2,33 +2,33 @@
 
 module Session where
 
-import Control.Concurrent.MVar
-import qualified Data.HashMap.Strict as Map
+import           Control.Concurrent.MVar
+import           Control.Monad.State
+import           Control.Lens
+import qualified Data.HashMap.Strict           as Map
 
-import Types
+import           Types
 
 initCacheStore :: IO CacheStore
 initCacheStore = newMVar Map.empty
 
 allValues :: CacheStore -> IO [IDPData]
 allValues store = do
-    m1 <- tryReadMVar store
-    return $ maybe [] Map.elems m1
+  m1 <- tryReadMVar store
+  return $ maybe [] Map.elems m1
 
 removeKey :: CacheStore -> IDPLabel -> IO ()
 removeKey store idpKey = do
-    m1 <- takeMVar store
-    let m2 = Map.update updateIdpData idpKey m1
-    putMVar store m2
-    where updateIdpData idpD = Just $ idpD { loginUser = Nothing }
+  m1 <- takeMVar store
+  let m2 = Map.update updateIdpData idpKey m1
+  putMVar store m2
+  where updateIdpData idpD = Just $ idpD { loginUser = Nothing }
 
 lookupKey :: CacheStore -> IDPLabel -> IO (Maybe IDPData)
 lookupKey store idpKey = do
-    m1 <- tryReadMVar store
-    return (Map.lookup idpKey =<< m1)
+  m1 <- tryReadMVar store
+  return (Map.lookup idpKey =<< m1)
 
 insertIDPData :: CacheStore -> IDPData -> IO ()
-insertIDPData store val = do
-    m1 <- takeMVar store
-    let m2 = Map.insert (idpDisplayLabel val) val m1
-    putMVar store m2
+insertIDPData store val =
+  modifyMVar_ store $ execStateT $ at (idpDisplayLabel val) ?= val
